47f864cf22a8c63f03530d53f9c64b308c844049
AnonyBSP <matheusgagliotti@hotmail.com>
2016-11-24 20:01:27 -0200

    Add files via upload

---
+++ responsive-design-master/responsive-design-master/Site/Pagina 2/funcao.js
@@ -0,0 +1,29 @@
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+  // Target (this) element is the source node.
+  this.style.opacity = '0.4';
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+Para ajudar, dataTransfer também possui um getData(format) para buscar os dados do arraste por mimetype. Esta é a modificação para processar a ação de soltar a coluna:
+
+function handleDrop(e) {
+  // this/e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // Stops some browsers from redirecting.
+  }
+
+  // Don't do anything if dropping the same column we're dragging.
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the columnwe dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
